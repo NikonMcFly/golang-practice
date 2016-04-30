e172181a22fb01ff03afce2d04dd214e6de703f2
nikonmcfly <nikonmcfly@gmail.com>
2016-04-30 17:44:14 -0500

    fixing file set up

---
+++ fibonacci/main.go
@@ -0,0 +1,15 @@
+package main
+
+
+
+import (
+	"fmt"
+)
+// the first two numbers in the
+// Fibonacci sequence are either 1 and 1, 
+// or 0 and 1, depending on the chosen starting
+//  point of the sequence, and each subsequent 
+//  number is the sum of the previous two.
+func fib(){
+
+}
